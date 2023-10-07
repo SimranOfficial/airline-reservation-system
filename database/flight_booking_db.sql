0c7df63e1ad6c96b6eaaa54cc672947c6ae02c60
Simran <simranchauhan272001@gmail.com>
2023-10-07 11:38:51 +0530

    initial commit

---
+++ database/flight_booking_db.sql
@@ -0,0 +1,247 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 23, 2022 at 04:48 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `flight_booking_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `airlines_list`
+--
+
+CREATE TABLE `airlines_list` (
+  `id` int(30) NOT NULL,
+  `airlines` text NOT NULL,
+  `logo_path` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `airlines_list`
+--
+
+INSERT INTO `airlines_list` (`id`, `airlines`, `logo_path`) VALUES
+(1, 'AirAsia India', '1600999080_kisspng-flight-indonesia-airasia-airasia-japan-airline-tic-asia-5abad146966736.8321896415221927106161.jpg'),
+(2, 'Philippine Airlines', '1600999200_Philippine-Airlines-Logo.jpg'),
+(3, 'Cebu Pacific', '1600999200_43cada0008538e3c1a1f4675e5a7aabe.jpeg'),
+(4, 'Air India', '1653313320_air_indfia.jpg'),
+(5, 'Air India Express', '1653313380_v7sbgynhrjwfurlpneql.webp');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `airport_list`
+--
+
+CREATE TABLE `airport_list` (
+  `id` int(30) NOT NULL,
+  `airport` text NOT NULL,
+  `location` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `airport_list`
+--
+
+INSERT INTO `airport_list` (`id`, `airport`, `location`) VALUES
+(2, 'Chhatrapati Shivaji Maharaj International Airport', 'Mumbai, India'),
+(3, 'Los Angeles International Airport', 'Los Angeles, California'),
+(4, 'Dubai International Airport', 'Garhoud, Dubai'),
+(5, 'Indira Gandhi International Airport', 'Delhi, India');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booked_flight`
+--
+
+CREATE TABLE `booked_flight` (
+  `id` int(30) NOT NULL,
+  `flight_id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `address` text NOT NULL,
+  `contact` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `booked_flight`
+--
+
+INSERT INTO `booked_flight` (`id`, `flight_id`, `name`, `address`, `contact`) VALUES
+(4, 5, 'Swati', 'Haridwar', '9045111111');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `flight_list`
+--
+
+CREATE TABLE `flight_list` (
+  `id` int(30) NOT NULL,
+  `airline_id` int(30) NOT NULL,
+  `plane_no` text NOT NULL,
+  `departure_airport_id` int(30) NOT NULL,
+  `arrival_airport_id` int(30) NOT NULL,
+  `departure_datetime` datetime NOT NULL,
+  `arrival_datetime` datetime NOT NULL,
+  `seats` int(10) NOT NULL DEFAULT 0,
+  `price` double NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `flight_list`
+--
+
+INSERT INTO `flight_list` (`id`, `airline_id`, `plane_no`, `departure_airport_id`, `arrival_airport_id`, `departure_datetime`, `arrival_datetime`, `seats`, `price`, `date_created`) VALUES
+(5, 4, '123', 2, 5, '2022-05-26 19:17:00', '2022-05-26 22:17:00', 80, 4000, '2022-05-23 19:17:43'),
+(6, 5, '568', 2, 4, '2022-05-30 15:17:00', '2022-05-24 01:18:00', 75, 15000, '2022-05-23 19:18:25'),
+(7, 2, '9785', 3, 2, '2022-06-03 19:18:00', '2022-06-04 09:18:00', 100, 30000, '2022-05-23 19:19:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `cover_img` text NOT NULL,
+  `about_content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `cover_img`, `about_content`) VALUES
+(1, 'Airline Reservation System', 'sample@gmail.com', '+910000000000', '1653313980_254361.jpg', '&lt;p style=&quot;text-align: center;&quot;&gt;&lt;b style=&quot;text-align: center; color: rgb(51, 51, 51);&quot;&gt;&lt;span style=&quot;color:rgb(255,0,0);text-align: center;&quot;&gt;AIRLINE RESEVATION SYSTEM&lt;/span&gt;&lt;br&gt;developed by&lt;br&gt;&lt;span style=&quot;color:rgb(255,0,0);text-align: center;&quot;&gt;SIMRAN, SONALI, SANIYA KAZMI &amp;amp; SWATI&lt;/span&gt;&lt;/b&gt;&lt;/p&gt;');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `doctor_id` int(30) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `address` text NOT NULL,
+  `contact` text NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1=admin , 2 = doctor,3=patient'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `doctor_id`, `name`, `address`, `contact`, `username`, `password`, `type`) VALUES
+(1, 0, 'Simran', '', '', 'simran', 'password', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `airlines_list`
+--
+ALTER TABLE `airlines_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `airport_list`
+--
+ALTER TABLE `airport_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `booked_flight`
+--
+ALTER TABLE `booked_flight`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `flight_list`
+--
+ALTER TABLE `flight_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `airlines_list`
+--
+ALTER TABLE `airlines_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `airport_list`
+--
+ALTER TABLE `airport_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `booked_flight`
+--
+ALTER TABLE `booked_flight`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `flight_list`
+--
+ALTER TABLE `flight_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
